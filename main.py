210d2df65925bcca92248e3df2d47819750c1861
Donghyun Lee <leedonghyun022@gmail.com>
2024-11-20 16:45:33 +0900

    chore: 포맷팅 추가

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import base64
-from fastapi import FastAPI, HTTPException, WebSocket
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Query, WebSocket
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -10,32 +11,33 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-
 )
 
-user_tokens= {
+user_tokens = {
     # "username,16 length random"
     "lee-donghyun,adsfasdfasdfasdf"
 }
 
 
 class Auth(BaseModel):
-    token:str
+    token: str
+
 
 @app.post("/authorize")
-async def authorize(auth:Auth):
+async def authorize(auth: Auth):
     if auth.token in user_tokens:
         return auth.token
     else:
-        raise HTTPException(status_code=401,detail="유효하지 않은 토큰입니다.")
-    
-@app.websocket('/stream')
-async def stream(websocket: WebSocket):
+        raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+
+
+@app.websocket("/stream")
+async def stream(websocket: WebSocket, token: Annotated[str | None, Query()] = None):
+    # message 에 토큰 포함하도록, 그 토큰 디렉토리에 파일 검사하도록.
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
         video_bytes = base64.b64decode(data)
-        with open('received_chunk.mp4', 'ab') as f:
+        with open("received_chunk.mp4", "wb") as f:
             f.write(video_bytes)
-            f.close()
         print("Received a video chunk")
